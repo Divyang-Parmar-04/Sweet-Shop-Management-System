37bfa2539e96bb588120a57072e9e1d8097417e9
divyang parmar <parmar84divyang@gmail.com>
2025-07-16 23:44:09 +0530

    ADD Search Sweet Test

---
+++ BackEnd/tests/searchSweet.test.js
@@ -0,0 +1,29 @@
+const request = require('supertest');
+const express = require('express');
+const sweetRoutes = require('../router/sweetsRoutes.js');
+const { setSweets } = require('../model/sweetsModel.js');
+
+const app = express();
+app.use(express.json());
+app.use('/api/sweets', sweetRoutes);
+
+describe('Sweet API Tests', () => {
+
+  // SET DUMMY TEST DATA 
+
+  beforeEach(() => {
+    setSweets([
+      { id: '1', name: 'Choco Lava', category: 'Chocolate', price: 55, stock: 10 },
+      { id: '2', name: 'Candy Pop', category: 'Candy', price: 10, stock: 20 }
+    ]);
+  });
+  // SEARCH SWEETs BASED ON NAME  TEST
+
+  test('Search sweets by name', async () => {
+    const res = await request(app).get('/api/sweets/search/choco');
+    expect(res.statusCode).toBe(200);
+    expect(res.body.length).toBeGreaterThan(0);
+    expect(res.body[0].name.toLowerCase()).toContain('choco');
+  });
+  
+})
